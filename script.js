8b3865e62fe086577a3a03d65c39e7383b054572
cblystone67 <christopherblystone1@gmail.com>
2023-04-03 22:02:23 -0700

    WIP: Shuffle working

--- script.js
+++ script.js
@@ -24,7 +24,14 @@ let deck = getDeck();
     }
     return deck;
   }
-  console.log(deck);
+  function shuffle(deck){
+    for(let i = deck.length; i > 0; i--){
+        let j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+  }
+  console.log(shuffle(deck));
   
 
 
