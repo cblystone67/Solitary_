29e75ddcb23399a9729f376fe5c97f71b29e2dbf
cblystone67 <christopherblystone1@gmail.com>
2023-04-05 11:49:28 -0700

    Developing board

--- script.js
+++ script.js
@@ -54,26 +54,3 @@ class Deck{
 
 
   /*----- functions -----*/
-
-  // function getDeck(){
-  //   let deck = new Array();
-  //   for(let i = 0; i < suits.length; i++){
-  //       for(let j = 0; j< values.length; j++){
-  //           let card = {Value: values[j], Suit: suits[i]};
-  //           deck.push(card);
-  //       }
-  //   }
-  //   return deck;
-  // }
-  // function shuffle(deck){
-  //   for(let i = deck.length; i > 0; i--){
-  //       let j = Math.floor(Math.random() * (i + 1));
-  //       [deck[i], deck[j]] = [deck[j], deck[i]];
-  //   }
-  //   return deck;
-  // }
-
-  // console.log(shuffle(deck));
-  
-
-
