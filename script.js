a7a0f166063df46be9e85cb9d59d43cd0b5e67ef
cblystone67 <christopherblystone1@gmail.com>
2023-04-06 21:42:47 -0700

    Got cards to load

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-  /*----- constants -----*/ 
-const Suits = ['hearts', 'spades', 'clubs', 'diamonds'];
-const Values = ["A", "r02", "r03", "r04", "r05", "r06", "r07", "r08", "r09", "r10", "J", "Q", "K"];
+/*----- constants -----*/
+
+const pile1El = document.getElementById('pile1');
+const pile2El = document.getElementById('pile2');
+const pile3El = document.getElementById('pile3');
+const pile4El = document.getElementById('pile4');
+const pile5El = document.getElementById('pile5');
+const pile6El = document.getElementById('pile6');
+const pile7El = document.getElementById('pile7');
+const deckEl = document.getElementById('deck');
+
+
+
+const Suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+const Values = ['A','K','Q','J','r10','r09','r08','r07','r06','r05','r04','r03','r02'];
+let deck;
+
 class Deck{
   constructor(cards = freshDeck()){
     this.cards = cards;
@@ -16,6 +30,9 @@ class Deck{
       this.cards[newIndex] = this.cards[i];
       this.cards[i] = oldValue;
     }
+  }
+  deal(numberOfCards){
+    return this.cards.splice(0, numberOfCards);
   }
 }
 class Card{
@@ -34,140 +51,39 @@ function freshDeck(){
 }
 
 
+function initialize(){
+  deck = new Deck();
+  deck.shuffle();
+  setPiles();
+}
+function setPiles(){
+  //1 We want it to deal one card face to pile one itterating through 
+  //adding 1 for each succeeding row.
+  let imageEl = document.createElement('img')
+  imageEl.setAttribute('src', deck.cards[0].imageUrl);
+  pile1El.appendChild(imageEl);
+  let imageEl2 = document.createElement('img')
+  imageEl2.setAttribute('src', deck.cards[1].imageUrl);
+  pile2El.appendChild(imageEl2);
+  let imageEl3 = document.createElement('img')
+  imageEl3.setAttribute('src', deck.cards[2].imageUrl);
+  pile3El.appendChild(imageEl3);
+  let imageEl4 = document.createElement('img')
+  imageEl4.setAttribute('src', deck.cards[3].imageUrl);
+  pile4El.appendChild(imageEl4);
+  let imageEl5 = document.createElement('img')
+  imageEl5.setAttribute('src', deck.cards[4].imageUrl);
+  pile5El.appendChild(imageEl5);
+  let imageEl6 = document.createElement('img')
+  imageEl6.setAttribute('src', deck.cards[5].imageUrl);
+  pile6El.appendChild(imageEl6);
+  let imageEl7 = document.createElement('img')
+  imageEl7.setAttribute('src', deck.cards[6].imageUrl);
+  pile7El.appendChild(imageEl7);
+}
 
 
-
-let deck = new Deck();
-deck.shuffle();
+// const deck = new Deck();
+// deck.shuffle();
 console.log(deck);
-
-
-
-
-
-
-
-
-
-//     deck = [];
-//     for(let suit of suits){
-//       for(let value of values){
-//         const card = document.createElement('div');
-//         card.classList.add('card', suit);
-//         card.dataset.suit = suit;
-//         card.dataset.value = value;
-//         const img = document.createElement('img');
-//         img.src = `deck/images/${suit}/${suit}-${value}.svg`;
-//         card.append(img);
-//         deck.push(card);
-//       }
-//     }
-//     return deck;
-// }
-
-// deck = createDeck();
-// function shuffle(deck){
-//   for (let i = deck.length - 1; i > 0; i--){
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [deck[i], deck[j]] = [deck[j], deck[i]];
-//   }
-//   return deck;
-// }
-// function dealCards(){
-//   const columns = document.querySelectorAll('.column');
-//   const cards = shuffle(createDeck());
-//   let cardIndex = 0;
-//   const numCols = 7;
-//   for (let i = 0; i < numCols; i++){
-//     for (let j = i; j < numCols; j++){
-//       const card = cards.splice(cardIndex, 1)[0];
-//       columns[j].appendChild(card);
-//       if (j === i){
-//         card.classList.add('face-up');
-//       }
-//       cardIndex++;
-//     }
-//   }
-// }
-// function handleCardClick(event) {
-//   const card = event.currentTarget;
-//   const column = card.parentNode;
-//   const topCard = column.lastElementChild;
-//   if (card === topCard) {
-//     card.classList.toggle('selected');
-//   } else if (topCard && topCard.classList.contains('face-up')) {
-//     const isValidMove = checkValidMove(card, topCard);
-//     if (isValidMove) {
-//       moveCard(card, topCard);
-//     }
-//   }
-// }
-// function checkValidMove(card, topCard) {
-//   const cardValue = Number(card.dataset.value);
-//   const topCardValue = Number(topCard.dataset.value);
-//   const cardColor = card.dataset.suit === 'hearts' || card.dataset.suit === 'diamonds' ? 'red' : 'black';
-//   const topCardColor = topCard.dataset.suit === 'hearts' || topCard.dataset.suit === 'diamonds' ? 'red' : 'black';
-//   const isOppositeColor = cardColor !== topCardColor;
-//   const isOneLess = cardValue === topCardValue - 1;
-//   return isOppositeColor && isOneLess;
-// }
-// function moveCard(card, topCard) {
-//   const fromColumn = card.parentNode;
-//   const toColumn = topCard.parentNode;
-//   const siblings = card.nextElementSibling ? Array.from(card.nextElementSibling.parentNode.children) : [];
-//   const cardsToMove = siblings.slice(siblings.indexOf(card));
-//   const placeholder = document.createElement('div');
-//   placeholder.classList.add('card', 'placeholder');
-//   toColumn.insertBefore(placeholder, topCard);
-//   fromColumn.removeChild(card);
-//   toColumn.insertBefore(card, placeholder);
-//   toColumn.removeChild(placeholder);
-//   cardsToMove.forEach(c => toColumn.appendChild(c));
-//   updateGameStatus();
-// }
-// function handleDropZoneDragEnter(event) {
-//   event.preventDefault();
-//   const dropZone = event.currentTarget;
-//   dropZone.classList.add('hovered');
-// }
-
-// function handleDropZoneDragLeave(event) {
-//   const dropZone = event.currentTarget;
-//   dropZone.classList.remove('hovered');
-// }
-
-// function handleDropZoneDragOver(event) {
-//   event.preventDefault();
-// }
-
-// function handleCardDragStart(event) {
-//   event.dataTransfer.setData('text/plain', event.currentTarget.id);
-//   event.currentTarget.classList.add('dragged');
-// }
-
-// function handleCardDragEnd(event) {
-//   event.currentTarget.classList.remove('dragged');
-// }
-// function handleDropZoneDrop(event) {
-//   event.preventDefault();
-//   const dropZone = event.currentTarget;
-//   const cardId = event.dataTransfer.getData('text/plain');
-//   const card = document.getElementById(cardId);
-
-//   if (dropZone.classList.contains('tableau') && card.parentElement.classList.contains('tableau')) {
-//     const tableauCards = Array.from(card.parentElement.children);
-//     const cardIndex = tableauCards.indexOf(card);
-//     const validMove = isTableauMoveValid(tableauCards.slice(cardIndex));
-//     if (validMove) {
-//       dropZone.appendChild(card);
-//     }
-//   } else if (dropZone.classList.contains('foundation') && card.parentElement.classList.contains('tableau')) {
-//     const foundationSuit = dropZone.classList[1];
-//     const foundationCards = getFoundationCards(foundationSuit);
-//     const validMove = isFoundationMoveValid(foundationCards, card);
-//     if (validMove) {
-//       dropZone.appendChild(card);
-//       removeCardFromTableau(card);
-//     }
-//   }
-// }
+initialize();
